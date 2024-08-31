49e253f33fbec95fb53a398062f12360432f3943
João Pedro <joao.pcanhete@gmail.com>
2024-08-31 20:37:23 -0300

    Document entity created

---
+++ internal-support/src/main/java/app/entity/Document.java
@@ -0,0 +1,49 @@
+package app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Document {
+
+    @Id
+    @GeneratedValue(GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    @NotNull
+    private Department department;
+
+    @NotNull
+    private Category category;
+
+    @NotNull
+    private Timestamp createdAt;
+
+    @NotNull
+    private Timestamp updatedAt;
+
+    @NotNull
+    private User user;
+
+}
