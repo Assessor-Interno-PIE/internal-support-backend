46d16edef56f147d322b3d4b7fec24e675702f98
João Pedro <joao.pcanhete@gmail.com>
2024-08-31 20:56:14 -0300

    User entity created

--- internal-support/src/main/java/app/entity/User.java
+++ internal-support/src/main/java/app/entity/User.java
@@ -1,4 +1,45 @@
 package app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotNull
+    private Department department;
+
+    private List<Document> viewed;
+
+    @NotNull
+    private AccessLevel accessLevel;
+    
 }
