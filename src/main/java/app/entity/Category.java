e893d6880a8a5be465d45dbfea63a14823dc64c2
João Pedro <joao.pcanhete@gmail.com>
2024-09-18 21:16:59 -0300

    Test development for the Entity

--- src/main/java/app/entity/Category.java
+++ src/main/java/app/entity/Category.java
@@ -22,8 +22,8 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "O campo 'nome' não pode estar em branco.")
-    @Pattern(regexp = "^[A-Za-zÀ-ÿ\\s]+$", message = "O campo 'nome' deve conter apenas letras.")
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Pattern(regexp = "^[A-Za-zÀ-ÿ\\s]+$", message = "O nome deve conter apenas letras")
     private String name;
 
     @OneToMany(mappedBy = "category")
