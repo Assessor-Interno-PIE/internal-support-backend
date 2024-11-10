8b03b1e618fcbce383614bf1e226fdeb23be4141
conradocmatheus <conrado.matheus@protonmail.com>
2024-11-10 16:52:20 -0300

    feat(back-end): added crossorigin to authcontroller

--- src/main/java/app/controller/AuthController.java
+++ src/main/java/app/controller/AuthController.java
@@ -4,11 +4,9 @@ import app.entity.User;
 import app.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
