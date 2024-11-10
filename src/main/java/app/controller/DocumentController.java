9756bb3e96fc133c2bca6240ec469b3acbe3f34a
João Graciotim <graciotim92@gmail.com>
2024-11-09 21:02:20 -0300

    feat: added endpoint /view/{documentId} to return docuemnt for browser viewing

--- src/main/java/app/controller/DocumentController.java
+++ src/main/java/app/controller/DocumentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -36,6 +37,24 @@ public class DocumentController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao salvar o arquivo: " + e.getMessage());
         }
     }
+
+    @GetMapping("/view/{documentId}")
+    public ResponseEntity<Resource> viewDocument(@PathVariable Long documentId) {
+        try {
+            Resource resource = documentService.downloadFile(documentId);
+
+            // Configurar o cabeçalho para exibir o PDF no navegador
+            String contentDisposition = "inline; filename=\"" + resource.getFilename() + "\"";
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                    .contentType(MediaType.APPLICATION_PDF) // Definir o tipo de conteúdo como PDF
+                    .body(resource);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
 
     @GetMapping("/download/{documentId}")
     public ResponseEntity<Resource> downloadDocument(@PathVariable Long documentId) {
