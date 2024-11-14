e4433d5919badcfd5ab7eeb2328c7c4d9836d40c
conradocmatheus <conrado.matheus@protonmail.com>
2024-11-14 18:21:35 -0300

    refactor(back-end): implemented UserDetails in user

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -11,7 +11,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -19,7 +24,15 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        String role = (this.isAdmin == 1) ? "ROLE_ADMIN" : "ROLE_USER";
+        authorities.add(new SimpleGrantedAuthority(role));
+        return authorities;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,4 +54,26 @@ public class User {
 
     @NotNull(message = "isAdmin é inválido")
     private int isAdmin;
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
