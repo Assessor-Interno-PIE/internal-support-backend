69903a8d961407e80a64b3ac4b4a53d3330673eb
conradocmatheus <conrado.matheus@protonmail.com>
2024-11-15 00:36:36 -0300

    style(back): removed unused imports

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -1,12 +1,9 @@
 package app.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
