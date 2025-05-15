4b0c6e6c78c1017e1af29f389d09e4bbacf18bf8
conradocmatheus <conrado.matheus@protonmail.com>
2025-05-15 15:02:17 -0300

    feat: added .env

--- src/main/java/app/InternalSupportApplication.java
+++ src/main/java/app/InternalSupportApplication.java
@@ -1,5 +1,6 @@
 package app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InternalSupportApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure().directory("src/main/resources").load();
 		SpringApplication.run(InternalSupportApplication.class, args);
 	}
 
