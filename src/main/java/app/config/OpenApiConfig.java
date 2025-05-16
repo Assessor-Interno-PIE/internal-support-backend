d531f49bacb982fe43aae07e315f1094ae4594a9
conradocmatheus <conrado.matheus@protonmail.com>
2025-05-16 20:24:54 -0300

    feat: managed to access group endpoints, testing now

--- src/main/java/app/config/OpenApiConfig.java
+++ src/main/java/app/config/OpenApiConfig.java
@@ -1,20 +1,34 @@
 package app.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${spring.security.oauth2.client.provider.internal-support.issuer-uri}")
+    private String issuerUri;
+
+    @Value("${spring.security.oauth2.client.registration.internal-support.client-id}")
+    private String clientId;
+
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "OAuth2";
+        String authUrl = issuerUri + "/protocol/openid-connect/auth";
+        String tokenUrl = issuerUri + "/protocol/openid-connect/token";
+
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("bearer-key")) // <- PARA O SWAGGER
                 .info(new Info()
                         .title("Internal Support API")
                         .version("1.0")
@@ -22,9 +36,20 @@ public class OpenApiConfig {
                         .contact(new Contact()
                                 .name("Suporte")
                                 .email("graciota@gmail.com")))
-                .schemaRequirement("bearer-key", new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT"));
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .type(SecurityScheme.Type.OAUTH2)
+                                .flows(new OAuthFlows()
+                                        .authorizationCode(new OAuthFlow()
+                                                .authorizationUrl(authUrl)
+                                                .tokenUrl(tokenUrl)
+                                                .scopes(new Scopes()
+                                                        .addString("openid", "OpenID Connect")
+                                                        .addString("profile", "Profile")
+                                                        .addString("email", "Email"))))
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .name(securitySchemeName)));
     }
 }
--- src/main/java/app/controller/GroupController.java
+++ src/main/java/app/controller/GroupController.java
@@ -5,6 +5,7 @@ import app.service.GroupService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/keycloak")
 @Tag(name = "Grupos Keycloak", description = "Endpoints para gerenciamento de grupos no Keycloak")
+@SecurityRequirement(name = "OAuth2")
 public class GroupController {
 
     @Autowired
