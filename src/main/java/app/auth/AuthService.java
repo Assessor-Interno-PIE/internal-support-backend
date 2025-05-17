e1292ce5afc7d607f330bebe8184371189d21b1b
felipe <felipecsena877@gmail.com>
2025-05-17 01:00:33 -0300

    deleted jwt classes and added keycloak dependency

--- src/main/java/app/auth/AuthService.java
+++ src/main/java/app/auth/AuthService.java
@@ -7,8 +7,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import app.config.JwtServiceGenerator;
-
 @Service
 public class AuthService {
 	
