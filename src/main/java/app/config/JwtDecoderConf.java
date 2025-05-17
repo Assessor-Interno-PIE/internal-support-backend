a35eb9ea6dfaa7859b02b7d7c09cffdab6aa2dd0
felipe <felipecsena877@gmail.com>
2025-05-17 17:02:21 -0300

    config for security and jwtdecoder adjusted

---
+++ src/main/java/app/config/JwtDecoderConf.java
@@ -0,0 +1,16 @@
+package app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+public class JwtDecoderConf {
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("https://localhost:8080/realms/grupo_05/protocol/openid-connect/certs")
+                .build();
+    }
+}
